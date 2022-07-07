5c963bb9a52ae9ae09c8fef21faa6ad98199c855
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-07-07 12:29:39 +0300

    refactor: types

---
+++ registries-app/src/types.d.ts
@@ -0,0 +1,34 @@
+interface ItemNews {
+  id: number;
+  title: string;
+  date: string;
+  text: string;
+  imageSrc: string;
+}
+
+interface Registry {
+  id: number;
+  name: string;
+  code: string;
+  classSW: string;
+  dataReg: string;
+  link: string;
+}
+
+interface User {
+  id: number;
+  firstName: string;
+  lastName?: string;
+  patronymic?: string;
+  country?: string;
+  city?: string;
+  phone?: string;
+  password: string;
+}
+
+interface AuthState {
+  isAuth: boolean;
+  currentUser: User | null;
+}
+
+export { ItemNews, Registry, User, AuthState,};
