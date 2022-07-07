b6539883af8d5788108bf4b8de218045ed918f20
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-07-07 12:30:25 +0300

    feat: add DB's

---
+++ registries-app/src/db/dbUsers.js
@@ -0,0 +1,25 @@
+const dbUsers = [
+  {
+    id: 0,
+    firstName: null,
+    password: null,
+    isAuth: false,
+  },
+  {
+    id: 1,
+    firstName: 'Admin',
+    password: 'admin',
+    isAuth: false,
+  },
+  {
+    id: 2,
+    firstName: 'Захар',
+    lastName: 'Палазник',
+    patronymic: 'Геннадьевич',
+    country: 'Беларусь',
+    city: 'Минск',
+    phone: '+375221113344',
+    password: '123123',
+    isAuth: false,
+  },
+];
